4b7f3867f7059a2745cd5538b920ead1591a7359
Jari Honkanen <jaarhonka@yahoo.com>
2024-08-15 08:58:51 +0300

    add instance of fpga_interconnect_pkg

--- efinity_spi_comm/top_trion.vhd
+++ efinity_spi_comm/top_trion.vhd
@@ -2,123 +2,8 @@ library ieee;
     use ieee.std_logic_1164.all;
     use ieee.numeric_std.all;
 
-package spi_communication_pkg is
-
-    type spi_receiver_record is record
-        cs_buffer          : std_logic_vector(2 downto 0);
-        spi_clock_buffer   : std_logic_vector(2 downto 0);
-        input_data_buffer  : std_logic_vector(15 downto 0);
-        output_data_buffer : std_logic_vector(15 downto 0);
-
-        i                      : natural range 0 to 4;
-        transmitted_data_index : natural range 0 to 15;
-        received_byte_index    : natural range 0 to 15;
-        byte_is_ready          : boolean;
-        received_byte          : std_logic_vector(7 downto 0);
-    end record;
-
-    constant init_spi_receiver : spi_receiver_record := (
-        (others => '0'), (others => '0'), (others => '0'), (others => '0'), 0, 0, 0, false, (others => '0'));
-
-    procedure create_spi_receiver (
-        signal self         : inout spi_receiver_record;
-        spi_cs              : in std_logic;
-        spi_clock           : in std_logic;
-        signal spi_data_out : out std_logic;
-        frame_out_of_spi    : in std_logic_vector(15 downto 0));
-
--------------------------------------------
-   function rising_edge_detected ( signal_buffer : std_logic_vector )
-       return boolean;
--------------------------------------------
-   function falling_edge_detected ( signal_buffer : std_logic_vector )
-       return boolean;
--------------------------------------------
-   function get_first_bit ( input : std_logic_vector)
-       return std_logic;
--------------------------------------------
-
-end package spi_communication_pkg;
-
-package body spi_communication_pkg is
-
-    procedure create_spi_receiver
-    (
-        signal self : inout spi_receiver_record;
-        spi_cs : in std_logic;
-        spi_clock : in std_logic;
-        signal spi_data_out : out std_logic;
-        frame_out_of_spi : in std_logic_vector(15 downto 0)
-    ) is
-    begin
-        self.spi_clock_buffer <= self.spi_clock_buffer(self.spi_clock_buffer'left-1 downto 0) & spi_clock;
-        self.cs_buffer        <= self.cs_buffer(self.cs_buffer'left-1 downto 0) & spi_cs;
-
-        if falling_edge_detected(self.cs_buffer) then
-            self.transmitted_data_index <= 1;
-            self.received_byte_index    <= 0;
-            self.output_data_buffer     <= frame_out_of_spi(frame_out_of_spi'left-1 downto 0) & '0';
-            spi_data_out                <= get_first_bit(std_logic_vector(frame_out_of_spi));
-        end if;
-
-        self.byte_is_ready <=false;
-        if falling_edge_detected(self.spi_clock_buffer) then
-            if self.transmitted_data_index < 8 then
-                self.transmitted_data_index <= self.transmitted_data_index + 1;
-            else
-                self.transmitted_data_index <= 0;
-                self.received_byte_index <= self.received_byte_index + 1;
-                self.byte_is_ready <=true;
-                self.received_byte <= self.output_data_buffer(6 downto 0) & '0';
-            end if;
-            self.output_data_buffer <= self.output_data_buffer(self.output_data_buffer'left-1 downto 0) & '0';
-            spi_data_out       <= self.output_data_buffer(self.output_data_buffer'left);
-        end if;
-        
-    end create_spi_receiver;
-
--------------------------------------------
-   function rising_edge_detected
-   (
-       signal_buffer : std_logic_vector 
-   )
-   return boolean
-   is
-   begin
-       return signal_buffer(signal_buffer'left downto signal_buffer'left-1) = "01";
-   end rising_edge_detected; 
-
--------------------------------------------
-   function falling_edge_detected
-   (
-       signal_buffer : std_logic_vector 
-   )
-   return boolean
-   is
-   begin
-       return signal_buffer(signal_buffer'left downto signal_buffer'left-1) = "10";
-   end falling_edge_detected; 
-
--------------------------------------------
-   function get_first_bit
-   (
-       input : std_logic_vector
-   )
-   return std_logic
-   is
-   begin
-       return input(input'left);
-       
-   end get_first_bit;
--------------------------------------------
-
-end package body spi_communication_pkg;
-
-library ieee;
-    use ieee.std_logic_1164.all;
-    use ieee.numeric_std.all;
-
     use work.spi_communication_pkg.all;
+    use work.fpga_interconnect_pkg.all;
 
 entity top is
     port (
@@ -143,34 +28,59 @@ architecture rtl of top is
     constant output_data : std15array :=(x"acdc", x"aaaa", x"5555", x"ffff", x"1234");
 
     signal self : spi_receiver_record := init_spi_receiver;
+    signal bus_from_main : fpga_interconnect_record := init_fpga_interconnect;
+    signal bus_from_test : fpga_interconnect_record := init_fpga_interconnect;
+
+    signal bus_to_main : fpga_interconnect_record := init_fpga_interconnect;
+    signal test_register : std_logic_vector(15 downto 0) := x"acdc";
 
 begin
 
     user_led <= ledstate;
 
-    test_spi : process(main_clock)
+------------------------------------------
+    main : process(main_clock)
     begin
         if rising_edge(main_clock) then
+            init_bus(bus_from_main);
 
             if falling_edge_detected(self.cs_buffer) then
                 testidata <= testidata + 3;
             end if;
 
             create_spi_receiver(self, spi_cs_in, spi_clock, spi_data_out, std_logic_vector(testidata));
-
             if rising_edge_detected(self.spi_clock_buffer) then
                 self.input_data_buffer  <= self.input_data_buffer(self.input_data_buffer'left-1 downto 0) & spi_data_in ;
             end if;
 
             if rising_edge_detected(self.cs_buffer) then
-                if self.input_data_buffer = x"acdc" then
-                    ledstate <= (others => '1');
-                else
-                    ledstate <= (others => '0');
-                end if;
+                CASE self.input_data_buffer is
+                    WHEN x"acdc" =>
+                        ledstate <= (others => '1');
+                        request_data_from_address(bus_from_main, 10);
+                    WHEN others =>
+                        ledstate <= (others => '0');
+                end CASE;
             end if;
 
         end if; --rising_edge
-    end process test_spi;	
+    end process main;
+------------------------------------------
+    test : process(main_clock)
+        
+    begin
+        if rising_edge(main_clock) then
+            bus_to_main <= bus_from_test;
+        end if; --rising_edge
+    end process test;	
+------------------------------------------
+    test_interconnect : process(main_clock)
+    begin
+        if rising_edge(main_clock) then
+            init_bus(bus_from_test);
+            connect_data_to_address(bus_from_main, bus_from_test, 10, test_register);
+        end if; --rising_edge
+    end process test_interconnect;	
 
+------------------------------------------
 end rtl;
---
+++ source/fpga_interconnect_pkg.vhd
@@ -0,0 +1,5 @@
+
+
+package fpga_interconnect_pkg is new work.fpga_interconnect_generic_pkg 
+    generic map(number_of_data_bits => 16,
+             number_of_address_bits => 16);
