80437f0348ffe3284c7f0b0a1b52897102461753
Jari Honkanen <jaarhonka@yahoo.com>
2024-08-15 07:59:02 +0300

    move spi_communication to own sourcefile and add fpga_communication submodule

---
+++ source/spi_receiver/spi_communication_pkg.vhd
@@ -0,0 +1,116 @@
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+package spi_communication_pkg is
+
+    type spi_receiver_record is record
+        cs_buffer          : std_logic_vector(2 downto 0);
+        spi_clock_buffer   : std_logic_vector(2 downto 0);
+        input_data_buffer  : std_logic_vector(15 downto 0);
+        output_data_buffer : std_logic_vector(15 downto 0);
+
+        i                      : natural range 0 to 4;
+        transmitted_data_index : natural range 0 to 15;
+        received_byte_index    : natural range 0 to 15;
+        byte_is_ready          : boolean;
+        received_byte          : std_logic_vector(7 downto 0);
+    end record;
+
+    constant init_spi_receiver : spi_receiver_record := (
+        (others => '0'), (others => '0'), (others => '0'), (others => '0'), 0, 0, 0, false, (others => '0'));
+
+    procedure create_spi_receiver (
+        signal self         : inout spi_receiver_record;
+        spi_cs              : in std_logic;
+        spi_clock           : in std_logic;
+        signal spi_data_out : out std_logic;
+        frame_out_of_spi    : in std_logic_vector(15 downto 0));
+
+-------------------------------------------
+   function rising_edge_detected ( signal_buffer : std_logic_vector )
+       return boolean;
+-------------------------------------------
+   function falling_edge_detected ( signal_buffer : std_logic_vector )
+       return boolean;
+-------------------------------------------
+   function get_first_bit ( input : std_logic_vector)
+       return std_logic;
+-------------------------------------------
+
+end package spi_communication_pkg;
+
+package body spi_communication_pkg is
+
+    procedure create_spi_receiver
+    (
+        signal self : inout spi_receiver_record;
+        spi_cs : in std_logic;
+        spi_clock : in std_logic;
+        signal spi_data_out : out std_logic;
+        frame_out_of_spi : in std_logic_vector(15 downto 0)
+    ) is
+    begin
+        self.spi_clock_buffer <= self.spi_clock_buffer(self.spi_clock_buffer'left-1 downto 0) & spi_clock;
+        self.cs_buffer        <= self.cs_buffer(self.cs_buffer'left-1 downto 0) & spi_cs;
+
+        if falling_edge_detected(self.cs_buffer) then
+            self.transmitted_data_index <= 1;
+            self.received_byte_index    <= 0;
+            self.output_data_buffer     <= frame_out_of_spi(frame_out_of_spi'left-1 downto 0) & '0';
+            spi_data_out                <= get_first_bit(std_logic_vector(frame_out_of_spi));
+        end if;
+
+        self.byte_is_ready <=false;
+        if falling_edge_detected(self.spi_clock_buffer) then
+            if self.transmitted_data_index < 8 then
+                self.transmitted_data_index <= self.transmitted_data_index + 1;
+            else
+                self.transmitted_data_index <= 0;
+                self.received_byte_index <= self.received_byte_index + 1;
+                self.byte_is_ready <=true;
+                self.received_byte <= self.output_data_buffer(6 downto 0) & '0';
+            end if;
+            self.output_data_buffer <= self.output_data_buffer(self.output_data_buffer'left-1 downto 0) & '0';
+            spi_data_out       <= self.output_data_buffer(self.output_data_buffer'left);
+        end if;
+        
+    end create_spi_receiver;
+
+-------------------------------------------
+   function rising_edge_detected
+   (
+       signal_buffer : std_logic_vector 
+   )
+   return boolean
+   is
+   begin
+       return signal_buffer(signal_buffer'left downto signal_buffer'left-1) = "01";
+   end rising_edge_detected; 
+
+-------------------------------------------
+   function falling_edge_detected
+   (
+       signal_buffer : std_logic_vector 
+   )
+   return boolean
+   is
+   begin
+       return signal_buffer(signal_buffer'left downto signal_buffer'left-1) = "10";
+   end falling_edge_detected; 
+
+-------------------------------------------
+   function get_first_bit
+   (
+       input : std_logic_vector
+   )
+   return std_logic
+   is
+   begin
+       return input(input'left);
+       
+   end get_first_bit;
+-------------------------------------------
+
+end package body spi_communication_pkg;
+
--- testbenches/spi_communication/spi_communication_tb.vhd
+++ testbenches/spi_communication/spi_communication_tb.vhd
@@ -18,7 +18,6 @@ architecture vunit_simulation of spi_communication_tb is
     package spi_transmitter_pkg is new work.spi_transmitter_generic_pkg generic map(g_clock_divider => 5);
     use spi_transmitter_pkg.all;
 
-
     constant clock_period      : time    := 1 ns;
     constant simtime_in_clocks : integer := 5000;
     
@@ -30,12 +29,15 @@ architecture vunit_simulation of spi_communication_tb is
 
     signal user_led : std_logic_vector(3 downto 0);
 
-    signal self : spi_transmitter_record := init_spi_transmitter;
+    signal spi_transmmitter : spi_transmitter_record := init_spi_transmitter;
 
     signal capture_buffer : std_logic_vector(15 downto 0);
     signal packet_counter : natural := 0;
 
     constant test_frame : bytearray :=(0 => x"ac", 1=> x"dc");
+
+    signal receive_buffer : std_logic_vector(7 downto 0) := (others => '0');
+
 
 begin
 
@@ -54,20 +56,40 @@ begin
 
     stimulus : process(simulator_clock)
 
+    ------------------------------------------------
+        procedure create_spi_master 
+        (
+            signal spi_transmmitter : inout spi_transmitter_record
+        )
+        is
+        begin
+            create_spi_transmitter(spi_transmmitter, spi_data_out);
+            if ready_to_receive_packet(spi_transmmitter) and packet_counter < test_frame'high  then
+                transmit_byte(spi_transmmitter, test_frame(1));
+                packet_counter <= packet_counter + 1;
+            end if;
+            
+        end create_spi_master;
+    ------------------------------------------------
     begin
         if rising_edge(simulator_clock) then
             simulation_counter <= simulation_counter + 1;
 
-            create_spi_transmitter(self, spi_data_out);
-
             CASE simulation_counter is
                 WHEN 50 => 
-                    transmit_byte(self, test_frame(0));
+                    transmit_byte(spi_transmmitter, test_frame(0));
                 WHEN others => --do nothing
             end CASE;
-            if ready_to_receive_packet(self) and packet_counter < 1  then
-                transmit_byte(self, test_frame(1));
-                packet_counter <= packet_counter + 1;
+
+            create_spi_master(spi_transmmitter);
+
+            if simulation_counter = 332 then
+                transmit_byte(spi_transmmitter, x"11");
+            end if;
+
+            if simulation_counter = 456 then
+                transmit_byte(spi_transmmitter, test_frame(0));
+                packet_counter <= 0;
             end if;
 
         end if; -- rising_edge
@@ -76,18 +98,18 @@ begin
     dut_top : entity work.top
     port map(
         main_clock   => simulator_clock           ,
-        spi_data_in  => self.spi_data_from_master ,
-        spi_clock    => self.spi_clock            ,
-        spi_cs_in    => self.spi_cs_in            ,
+        spi_data_in  => spi_transmmitter.spi_data_from_master ,
+        spi_clock    => spi_transmmitter.spi_clock            ,
+        spi_cs_in    => spi_transmmitter.spi_cs_in            ,
         spi_data_out => spi_data_out              ,
         user_led     => user_led
     );
 ------------------------------------------------------------------------
-    catch_spi : process(self.spi_clock)
-        
+    catch_spi : process(spi_transmmitter.spi_clock)
     begin
-        if rising_edge(self.spi_clock) then
-            capture_buffer <= capture_buffer(14 downto 0) & self.spi_data_from_master;
+        if rising_edge(spi_transmmitter.spi_clock) then
+            capture_buffer <= capture_buffer(14 downto 0) & spi_transmmitter.spi_data_from_master;
         end if; --rising_edge
     end process catch_spi;	
+------------------------------------------------------------------------
 end vunit_simulation;
